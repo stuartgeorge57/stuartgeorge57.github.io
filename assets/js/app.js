3a46f7dff94b5d03f2c589671af0a0fb83d04c33
Stuart <43421430+stuartgeorge57@users.noreply.github.com>
2019-11-16 17:24:47 +1300

    coordinate update

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,7 +79,7 @@ function clearHighlight() {
 
 function sidebarClick(id) {
   var layer = markerClusters.getLayer(id);
-  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 17);
+  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 20);
   layer.fire("click");
   /* Hide sidebar and go to the map on small screens */
   if (document.body.clientWidth <= 767) {
@@ -221,7 +221,7 @@ var markerClusters = new L.MarkerClusterGroup({
   spiderfyOnMaxZoom: true,
   showCoverageOnHover: false,
   zoomToBoundsOnClick: true,
-  disableClusteringAtZoom: 20
+  disableClusteringAtZoom: 18
 });
 
 /* Empty layer placeholder to add to layer control for listening when to add/remove naturetrail to markerClusters layer */
